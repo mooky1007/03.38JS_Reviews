dded27c30875cf8dc75c1c8d89bcd859f818f7ab
mooky1007 <mooky1007@naver.com>
2023-05-19 15:14:19 +0900

    update

--- review.js
+++ review.js
@@ -3,54 +3,47 @@ class Reviews {
         this.container = document.querySelector(target);
         this.wrapper = this.container.querySelector('.review_wrapper');
         this.reviewItems = this.container.querySelectorAll('.review_item');
-        this.currentIdx = 0;
         this.prevBtn = this.container.querySelector('[data-action="prev"]');
         this.nextBtn = this.container.querySelector('[data-action="next"]');
+        this.currentIdx = 0;
         this.init();
     }
 
     init() {
         this.reviewItems.forEach((item, idx) => {
             const itemWidth = item.offsetWidth;
-            item.style.left = `${itemWidth * idx}px`;
+            item.style.transform = `translateX(${itemWidth * idx}px)`;
         });
 
-        this.prevBtn.addEventListener('click', this.prev.bind(this));
-        this.nextBtn.addEventListener('click', this.next.bind(this));
+        this.prevBtn.addEventListener('click', this.goToPrev.bind(this));
+        this.nextBtn.addEventListener('click', this.goToNext.bind(this));
 
         this.render();
     }
 
-    next() {
-        if(this.currentIdx < this.reviewItems.length - 1) {
+    goToNext() {
+        if (this.currentIdx < this.reviewItems.length - 1) {
             this.currentIdx++;
         } else {
-            this.currentIdx = 0;
+            return;
         }
         this.render();
     }
 
-    prev() {
-        if(this.currentIdx > 0) {
+    goToPrev() {
+        if (this.currentIdx > 0) {
             this.currentIdx--;
         } else {
-            this.currentIdx = this.reviewItems.length - 1;
+            return;
         }
         this.render();
     }
 
     render() {
-        this.wrapper.style.left = `${-this.currentIdx * 100}%`;
-        if(this.currentIdx === 0) {
-            this.prevBtn.setAttribute('disabled', true);
-        } else {
-            this.prevBtn.removeAttribute('disabled');
-        }
+        const translateX = -this.currentIdx * 100;
+        this.wrapper.style.transform = `translateX(${translateX}%)`;
 
-        if(this.currentIdx === this.reviewItems.length - 1) {
-            this.nextBtn.setAttribute('disabled', true);
-        } else {
-            this.nextBtn.removeAttribute('disabled');
-        }
+        this.prevBtn.disabled = this.currentIdx === 0;
+        this.nextBtn.disabled = this.currentIdx === this.reviewItems.length - 1;
     }
 }
